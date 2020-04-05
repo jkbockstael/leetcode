0af41616c4494d3af3c99c9dc972631a01c92179
Jean-Karim Bockstael <jkb@jkbockstael.be>
2020-04-05 12:31:10 +0200

    Solution for Day 3, in Python

---
+++ 2020-04-month-long-challenge/problem003.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Day 3: Maximum Subarray
+#
+# Given an integer array nums, find the contiguous subarray (containing at least
+# one number) which has the largest sum and return its sum.
+
+class Solution:
+    def maxSubArray(self, nums: [int]) -> int:
+        total = 0
+        best_total = float("-inf")
+        for num in nums:
+            total = max(total + num, num)
+            best_total = max(total, best_total)
+        return best_total
+
+assert Solution().maxSubArray([-2,1,-3,4,-1,2,1,-5,4]) == 6, "[-2,1,-3,4,-1,2,1,-5,4] → 6"
+assert Solution().maxSubArray([-1]) == -1, "[-1] → -1"
