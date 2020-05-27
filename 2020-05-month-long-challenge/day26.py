aa11e79c4ac199951526d5677988309ce8e67747
Jean-Karim Bockstael <jkb@jkbockstael.be>
2020-05-27 09:12:34 +0200

    Solution for May Challenge Day 26, in Python

---
+++ 2020-05-month-long-challenge/day26.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Day 26: Contiguous Array
+#
+# Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+
+# This is the same problem as April Challenge Day 13
+class Solution:
+    def findMaxLength(self, nums: [int]) -> int:
+        counts = {0: -1}
+        longest = 0
+        count = 0
+        for i in range(len(nums)):
+            count += -1 if nums[i] == 0 else 1
+            if count not in counts:
+                counts[count] = i
+            else:
+                longest = max(longest, i - counts[count])
+        return longest
+
+# Test
+assert Solution().findMaxLength([0,1]) == 2
+assert Solution().findMaxLength([0,1,0]) == 2
+assert Solution().findMaxLength([0,0,0]) == 0
+assert Solution().findMaxLength([0,0,0,1,0,1,1]) == 6
