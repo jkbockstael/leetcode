0d731074c4f0c964b63bb90b154b02a86aed83a5
Jean-Karim Bockstael <jkb@jkbockstael.be>
2020-06-09 09:21:08 +0200

    Solution for June Challenge Day 8, in JavaScript

---
+++ 2020-06-month-long-challenge/day08.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env nodejs
+
+/*
+ * Day 8: Power of Two
+ *
+ * Given an integer, write a function to determine if it is a power of two.
+ */
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    // In JavaScript this is faster than return n > 0 && (n & (n - 1)) == 0
+    return n > 0 && n.toString(2).split('1').length == 2
+};
+
+console.assert(isPowerOfTwo(1) == true)
+console.assert(isPowerOfTwo(16) == true)
+console.assert(isPowerOfTwo(218) == false)
+console.assert(isPowerOfTwo(-16) == false)
