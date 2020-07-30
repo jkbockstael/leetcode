55c18f75dfc1a89f5980025bc428e5b27cda52e5
Jean-Karim Bockstael <jkb@jkbockstael.be>
2020-07-30 11:29:33 +0200

    Solution for July Challenge Day 29, in Python

---
+++ 2020-07-month-long-challenge/day29.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Day 29: Best Time to Buy and Sell Stock with Cooldown
+#
+# Say you have an array for which the ith element is the price of a given stock
+# on day i.
+# Design an algorithm to find the maximum profit. You may complete as many
+# transactions as you like (ie, buy one and sell one share of the stock
+# multiple times) with the following restrictions:
+# - You may not engage in multiple transactions at the same time (ie, you must
+#   sell the stock before you buy again).
+# - After you sell your stock, you cannot buy stock on next day. (ie, cooldown
+#   1 day)
+
+class Solution:
+    def maxProfit(self, prices: [int]) -> int:
+        if len(prices) == 0:
+            return 0
+        buy = [0 for _ in prices]
+        sell = [0 for _ in prices]
+        buy[0] = -prices[0]
+        for day in range(1, len(prices)):
+            buy[day] = max(buy[day - 1], \
+                (sell[day - 2] if day >= 2 else 0) - prices[day])
+            sell[day] = max(sell[day - 1], buy[day - 1] + prices[day])
+        return sell[-1]
+
+# Test
+assert Solution().maxProfit([1,2,3,0,2]) == 3
